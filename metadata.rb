6f9b8070c40f3c5543d3f949566c95ee1a889b20
Joe Stump <joe@joestump.net>
2013-12-16 17:42:27 -0800

    Removed Vagrantfile as you can't boot this cookbook into anything. Added the basic LWRP stuff.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'route53'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Joe Stump'
+maintainer_email 'joe@joestump.net'
 license          'All rights reserved'
 description      'Installs/Configures route53'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
