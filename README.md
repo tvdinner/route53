3b3a643b8821a0a0435c91b309dc7f63d60f344b
Joe Stump <joe@joestump.net>
2013-12-16 17:33:45 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# route53 cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: route53
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
